4b2ef0b27603c811a1f4cf9ff9867e6cb3f989b7
Joshua-Zou <joshyzou@gmail.com>
2021-10-31 20:50:07 -0700

    prevented previewer from rendering too many times/s

--- components/CodePreview.js
+++ components/CodePreview.js
@@ -12,11 +12,18 @@ class CodePreview extends tApp.Component {
 	render(props) {
 		function loadCoad(){
 			setTimeout(function(){
-				document.getElementById("preview").srcdoc = codeEditorHelper.getValue();
+				try {
+					document.getElementById("preview").srcdoc = codeEditorHelper.getValue();
+				}catch(err){
+					return loadCoad()
+				}
 				loadCoad();
 			}, 1000)
 		}
-		loadCoad()
+		if (window.startedRefresh !== true) {
+			loadCoad()
+		}
+		window.startedRefresh = true
 		return `<div style="margin-top: 45px; height: 95%; background: white"><iframe style="width: 98%; height: 100%" id="preview" srcdoc='Loading...'></iframe></div>`;
 	}
 }
