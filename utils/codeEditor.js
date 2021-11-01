8b7a5e380999c9781544707cca7588c0b7ea9274
Joshua-Zou <joshyzou@gmail.com>
2021-10-31 19:20:12 -0700

    created ways of accessing/changing the editor

--- utils/codeEditor.js
+++ utils/codeEditor.js
@@ -1,6 +1,14 @@
 const editorFunctions = {
     updateLanguage: function(language) {
-        window.monaco.editor.setModelLanguage(window.monaco.editor.getModels()[0], language)
+        document.getElementById("code-frame").contentWindow.monaco.editor.setModelLanguage(document.getElementById("code-frame").contentWindow.monaco.editor.getModels()[0], language)
+        return true;
+    },
+    updateContent: function(content) {
+        document.getElementById("code-frame").contentWindow.codeEditor.setValue(content);
+        return true
+    },
+    getValue: function() {
+        return document.getElementById("code-frame").contentWindow.codeEditor.getValue()
     }
 }
 
