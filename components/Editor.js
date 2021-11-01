35ee1f538f156d2f589b0f019ca26a7e6c84b6ee
Joshua-Zou <joshyzou@gmail.com>
2021-10-31 18:33:25 -0700

    Fixed multiple editors problem

--- components/Editor.js
+++ components/Editor.js
@@ -6,19 +6,23 @@ class Editor extends tApp.Component {
 		super(state, parent);
 	}
 	render(props) {
-		loadRequire.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.29.1/min/vs' } });
-		loadRequire(['vs/editor/editor.main'], function () {
-			window.codeEditor = monaco.editor.create(document.getElementById('code-container'), {
-				value: `<!DOCTYPE>
-
-<html>
-</html>
-					`,
-				language: 'html',
-				theme: "vs-dark",
-				automaticLayout: true
+		if (window.loadedEditor !== true) {
+			window.loadedEditor = true
+			loadRequire.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.29.1/min/vs' } });
+			loadRequire(['vs/editor/editor.main'], function () {
+				window.codeEditor = monaco.editor.create(document.getElementById('code-container'), {
+					value: `<!DOCTYPE>
+	
+	<html>
+	</html>
+						`,
+					language: 'html',
+					theme: "vs-dark",
+					automaticLayout: true
+				});
 			});
-		});
+		}
+
 		return `<div class="code-editor">
 			<div id="code-container" style="width:100%;height:96%;border:1px solid grey; margin-top: 20px"></div>
 			<script>
