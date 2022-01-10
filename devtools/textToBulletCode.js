4bfdb88acfb2e71d8bf2371f5969c80d436e95c4
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-01-09 16:59:58 -0800

    Week 7 lesson

--- devtools/textToBulletCode.js
+++ devtools/textToBulletCode.js
@@ -2,15 +2,26 @@
 NOTE: If you see a weird icon in terminal, don't worry. It's just because the terminal doesn't have the correct font package.
 Just copy everything, and it should be visible in VS CODE or you're favorite code editor
 */
+// var text = `
+// Almost exactly like HTML
+//     Few minor differences
+//     Differences allow for more functionality
+//     All HTML attributes get camelCased
+//         onclick turns into onClick
+// The user can display variables in JSX syntax
+//     Allows to dynamically render content
+// `
 var text = `
-We are using the async/await approach for this, and therefore, we are wrapping the statements in an async function
-We await the axios.post() call which takes in 3 parameters (we only used 2 here)
-The first parameter is the url endpoint of course
-The second parameter is the body object that we accessed in the req object earlier
-We are passing in the name, email, and username
-The last parameter which is optional, are the URL Headers, but we are focus on them later
+In React, the React Framework will render everything into an HTML file, typically a div with id “root” in the “index.html” file
+To render an HTML element in ReactJS, use 
+ReactDOM.render([HTML code], [HTML element describing where to render])
+In “main.jsx” (under the “src” folder), replace all of the code with the following:
+First we import all of the required
+NPM modules
+Then, we create an element and 
+ReactDOM.render it into the HTML file
+named “index.html”
 `
-
 
 String.prototype.replaceAll = function (find, replace) {
     var regex = new RegExp(find, 'g');
@@ -21,9 +32,21 @@ if (text.lastIndexOf("\n") === text.length - 1) text = text.slice(0, -1)
 text = text.split("\n")
 var code = []
 for (i = 0; i < text.length; i++) {
-    code.push({
-        type: "text",
-        content: "➔ "+text[i]
-    })
+    if (text[i].startsWith("        ")) {
+        code.push({
+            type: "text",
+            content: "[[ ]][[ ]][[ ]][[ ]]● "+text[i].slice(8)
+        })
+    }else if (text[i].startsWith("    ")) {
+        code.push({
+            type: "text",
+            content: "[[ ]][[ ]]◆ "+text[i].slice(4)
+        })
+    }else{
+        code.push({
+            type: "text",
+            content: "➔ "+text[i]
+        })
+    }
 }
 console.log(JSON.stringify(code).slice(1,-1))
