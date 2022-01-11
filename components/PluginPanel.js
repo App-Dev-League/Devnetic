5aa88c0ca6145b3022d7eb500bb7ce184369508f
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-01-10 17:48:54 -0800

    updated UI bug

--- components/PluginPanel.js
+++ components/PluginPanel.js
@@ -4,6 +4,10 @@ const plugins = require("../utils/plugins.js");
 class PluginPanel extends tApp.Component {
 	constructor(state, parent) {
 		super(state, parent);
+        this.state.pluginSizes = {};
+        plugins.availablePlugins().forEach(async plugin => {
+            this.state.pluginSizes[plugin.id] = await plugins.getDownloadSize(plugin.id);
+        })
 	}
     install(id){
         var pluginpanel = this;
@@ -24,6 +28,7 @@ class PluginPanel extends tApp.Component {
                 }
                 bytesReceived += result.value.length;
                 plugin.querySelector("h5").innerText = "Installing..."
+                plugin.querySelector("h5").style.pointerEvents = "none"
                 plugin.querySelector(".loading-bar-container").style.opacity = 1;
                 plugin.querySelector(".loading-bar").style.width = Math.round(bytesReceived/code_size*100)+"%";
             }
@@ -53,6 +58,7 @@ class PluginPanel extends tApp.Component {
                         <div>
                             <h3 style="margin-bottom: 0; margin-top: 0; display: inline-block">${plugin.name}</h3>
                             ${plugins.checkPluginStatus(plugin.id) ? `<h5 style="display: inline-block; margin: 0; margin-left: 30px; transform: translateY(-2px); font-size: 0.8em; padding: 0 5px; line-height: 14px; background-color: #b12c2c; cursor: pointer" onclick="{{_this}}.uninstall('${plugin.id}')">Uninstall</h5>` : `<h5 style="display: inline-block; margin: 0; margin-left: 30px; transform: translateY(-2px); font-size: 0.8em; padding: 0 5px; line-height: 14px; background-color: #2BA143; cursor: pointer" onclick="{{_this}}.install('${plugin.id}')">Install</h5>`}
+                            <h6 style="display: inline-block; margin: 0; margin-left: 20px">${Math.round(this.state.pluginSizes[plugin.id]/1000)} KB</h6>
                             </div>
                         <p style="margin-top: 0; margin-bottom: 0">${plugin.description}</p>
                         <div class="loading-bar-container" style="margin-top: 5px; opacity: 0">
