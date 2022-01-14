74779620927468ddd43e084b32816aa56b673cd5
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-01-14 10:38:00 -0800

    replaced images with codeblocks

--- devtools/codeToSingleLine.js
+++ devtools/codeToSingleLine.js
@@ -1,13 +1,17 @@
 var code = `
-class Component1 extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        this.props.onChangeParent("newStateValue")
-        return;
-    }
-}
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>My test page</title>
+</head>
+
+<body>
+  <p>This is my page</p>
+</body>
+
+</html>
 `
 
 if (code.startsWith("\n")) code = code.slice(1)
