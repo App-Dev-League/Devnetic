26bf6596aa5f9e7ccb613029bf38ccd425ab13cb
Sarthak-Dayal <63827830+Sarthak-Dayal@users.noreply.github.com>
2022-02-20 01:47:02 -0600

    Fix pretty printing bug

--- devtools/parseKahoot.py
+++ devtools/parseKahoot.py
@@ -1,8 +1,6 @@
 # Prints an array of all Kahoot questions converted to the ADL multiple_choice data object
 import json
 import requests
-import pprint
-pp = pprint.PrettyPrinter()
 
 # CHANGE URL TO YOUR KAHOOT
 url = 'https://create.kahoot.it/details/951f4c0e-b6d7-4aed-8853-cf0e82c2d3ca'
@@ -16,8 +14,8 @@ object = []
 for q in data['kahoot']['questions']:
     curr = {"type": "multiple_choice", "question": q['question'], "descriptions": [ "Desc1", "Desc2", "Desc3","Desc4"],"points": 5,"coins": 1, "answers": []}
     for choice in q['choices']:
-        curr["answers"].append(choice['answer'].replace('&nbsp;', ' '))
+        curr["answers"].append(choice['answer'].replace('&nbsp;', ' ').replace("''", ""))
         if choice['correct']:
             curr["correct"] = len(curr["answers"]) - 1
     object.append(curr)
-pp.pprint(object)
+print(object)
