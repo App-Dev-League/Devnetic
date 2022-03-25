be4d7be9e1d149b19c37f9edc6f02ce242ac9771
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-03-25 16:16:05 -0700

    webdev projects week 4

--- components/Congratulations.js
+++ components/Congratulations.js
@@ -15,8 +15,19 @@ class Congratulations extends ModuleComponent {
 	${(this.data().elements || []).map(element => {
 		if(element.type == "code") {
 			return `<div class="codeblock-wrapper">
-	${new codeBlock({code: codeBlockHelper.escapeHtml(element.content || ""), language: element.lang, name: element.name}, this)}
+			${new codeBlock({code: codeBlockHelper.escapeHtml(element.content || ""), language: element.lang, name: element.name})}
 </div>`
+		} else if (element.type == "divider"){
+			if (element.height) return `<div style="width: 100%; height: ${element.height}px; background-color: rgba(0,0,0,0)"></div>`;
+			else return `<div style="width: 100%; height: 20px; background-color: rgba(0,0,0,0)"></div>`;
+		} else if (element.type == "image"){
+			let styles = element;
+			if (!styles.width) styles.width = "90%"
+			let style = "";
+			Object.entries(styles).forEach(([key, value]) => {
+				if (key !== "src" && key !== "type") style += `${key}: ${value};`
+			})
+			return `<div class="image-wrapper info-text"><img src="${element.src}" style="display: block; margin-left: auto; margin-right: auto; ${style}"></div>`
 		} else {
 			return `<pre class="info-text">${codeTemplateToCode(element.content || "")}</pre>`;
 		}
