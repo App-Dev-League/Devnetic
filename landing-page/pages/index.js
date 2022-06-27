5797769608355b31344408aba9de45f12383b07b
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-06-27 14:34:48 -0700

    Fix build errors

--- landing-page/pages/index.js
+++ landing-page/pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import Signup from "./components/signup"
-import Navbar from "./components/navbar"
-import Footer from "./components/footer"
-import Details from "./components/Details"
-import Educators from "./components/Educators"
+import Signup from "../components/signup"
+import Navbar from "../components/navbar"
+import Footer from "../components/footer"
+import Details from "../components/Details"
+import Educators from "../components/Educators"
 
 export default function Home() {
   return (
