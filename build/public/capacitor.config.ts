80f6a800cff7a90d5cc060dcf54c3bc6e9d22af6
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-07-03 12:05:22 -0700

    test gradle github workflow

---
+++ build/public/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.appdevleague.devnetic',
+  appName: 'Devnetic',
+  webDir: './app',
+  bundledWebRuntime: false
+};
+
+export default config;
+
+
