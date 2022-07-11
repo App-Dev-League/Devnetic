a0677d072c6a70a918d034e721d31127ea4454a6
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-07-10 22:05:11 -0700

    Fix explanation modal bugs

--- src/components/ModulePage.js
+++ src/components/ModulePage.js
@@ -15,7 +15,7 @@ class ModulePage extends tApp.Component {
 		this.setState("component", component);
 	}
 	async next() {
-		if (document.getElementById("continue-button").classList.contains("continue-button-disabled")) return;
+		if (document.getElementById("continue-button") && document.getElementById("continue-button").classList.contains("continue-button-disabled")) return;
 		if(this.state.next != null) {
 			if(this.state.data.type == "snippet_unlock" && this.state.data.snippet != null) {
 				await this.state.Database.storeSnippet(this.state.data.snippet);
