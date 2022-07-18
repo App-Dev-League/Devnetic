c9919334de641fbf9bca79a3c502e59904a810d8
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-07-17 18:03:44 -0700

    Fix typos

--- devtools/spellcheck.js
+++ devtools/spellcheck.js
@@ -10,6 +10,7 @@ const customWords = [
     "javascript", "repl", "css", "html", "js", "html5", "htmlcssjs","php", "nodejs","replitcom","doctype", "charset", "seo", "h1",  "utf8",  "cssjavascript", "href", "lili", "ulul", "unpairedselfclosing", "lightpurplep", "ctrls", "bootstrap's", "cdn", "jsdelivr", "clientside", "serverside", "popups", "taskload", "dom", "bigint", "divs", "webpage", "parsers", "fontfamily", "textalign", "JSX", "frontend", "backend", "React's", "onclick", "br", "img", "ReactJS", "navbar", "HTMLJSX", "replit", "filesystem", "lifecycle", "keysvalue", "camelCased", "yourStuff", "forloop", "arraymap", "Devnetic", "NPM", "npm", "EJS", "axios", "async", "CORS", "Reachability", "req", "res", "eval", "ekey", "ie", "indexjs", "ExpressJS", "rerender", "rerenders", "init", "2D", "1D", "0s", "NLP", "LOF", "LRD",
     // machine-generated words
     ,'fontsize','Xoffset','Yoffset','rgbred','1s','3s','prebuilt','sm','CDNs','letvar','andor','bye',"'",'Booleans','y','jQuery','webpages','url','JQuery','initto','CommonJS','ES6','fs','Expressjs','APIs','ressend','filenamejs','reqbody','etcThese',"parametersaxiosgethttpsservercomendpointid123dogtype'",'reqquery','const',"SSEsSSE's",'localhost3001servertime','UI','ReactDOM','mainjsx','src','reusability','moodhi','getset','keysvalues','onscroll','onScroll','myFunction','onChange',"another's",'component1','headhead','bodybody','ul','li','ol','youtube','nav','libs','madLibs','Eg','del','clearVal','stylecss','calc','400px','JSON','num1','num2','testingjs','READMEmd','ok',"express'",'number1','number2','expressjs','templating','toString','dev','cors','Displayer','DevTools','ToDo','elementkey','addItem','AddItem',"whatever's",'textbox','ItemList','absx','seq1','seq2','lenseq','2Darray','ifthen','eb','elif','infinte','unindent','OOP','Prebuilt','Subfield','Labelled','unlabelled','YesNo','Unlabelled','SVMs','mx','m1x1','m2x2','m3x3','mnxn','ndimensional','yvalue','OLS','Vapnik','SVM','XGBoost','overfit','Sigmoid','ReLu','recommender','RGB','HSV','subfields','Embeddings','embeddings','Onehot','ELMo','subfield','finegrain','SMT','Rulebased','RBMT','HMT','NMT','Modelling','GPT3','modelling','RNNs','biLSTMs','RNN','MLM','NSP','onehot','PoS','biLSTMS','Kmeans','DBSCAN','Hebbian','GANs','KMeans','knearest','Kdistance','Kth','reachability','LOFs','codings','sklearn','pythagorean','isare','encodings'
+    ,'pHello','worldp','etcex','webpage\'s','pxyz','xyz','\'\'','10px','\'shadow\'','rgb','\'bye\'','00001Booleans','\'y\'','Mathmax','0You','toYou','pageSome','squar','package1','camelCases','classNames','linkCSS','240Explanation','overAdd','CSSAs','propsTo','pageAs','readyWe','Recode','absolutex','Myelin','Multiplicatives','0Activation','ReLuUsing','superset','CNNs','workWhy','workWord','k2','center4'
 ]
 
 String.prototype.replaceAll = function (find, replace) {
@@ -37,6 +38,15 @@ async function main(err, dict) {
                 if (!page.elements) return;
                 page.elements.forEach(element => {
                     if (element.type === "text") spellcheck(element.content, file, spell)
+                    if (element.type && element.type.endsWith("multiple_choice")) {
+                        spellcheck(element.question, file, spell)
+                        element.answers.forEach(answer => {
+                            spellcheck(answer, file, spell)
+                        })
+                        element.descriptions.forEach(descriptions => {
+                            spellcheck(descriptions, file, spell)
+                        })
+                    }
                 })
             })
         })
