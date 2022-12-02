2292c9181f7e3b615408dc669c3b3166b9796e13
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-12-02 09:17:52 -0800

    Fix bettereditor

--- src/utils/plugins.js
+++ src/utils/plugins.js
@@ -103,7 +103,7 @@ module.exports = {
                 image: "./assets/plugins/betterEditor/betterEditor.png",
                 id: "betterEditor",
                 onInstall: "plugins.load(\"betterEditor\").then(() => updateEditorSettings({dividerPosition: null}))",
-                latestVersion: "2.4.4"
+                latestVersion: "2.4.5"
                 //https://interactjs.io/
             },
             {
