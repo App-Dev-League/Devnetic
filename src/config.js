3ea1e401e33dcfda6685232b1f147c30630c60e0
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-12-05 16:15:12 -0800

    Add window recursion support

--- src/config.js
+++ src/config.js
@@ -465,7 +465,7 @@
 	window.newLinkWindow = function(link) {
 		event.preventDefault();
 		windows.newWindow("Devnetic Tab", `
-		<iframe src="${link}" style="width: 100%; height: 100%; border: none; margin-top: -2px" frameBorder="0" />`, window.innerWidth/2, window.innerHeight)
+		<iframe src="${link.replace("#/", "?interframe=true#/")}" style="width: 100%; height: 100%; border: none; margin-top: -2px" frameBorder="0" />`, window.innerWidth/2, window.innerHeight)
 	}
 })();
 function recurseEjs(html, parameters = {}) {
